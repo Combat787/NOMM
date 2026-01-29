827b5472a178498375e437353130a3f9ede6fe4a
Combat <787combat787@gmail.com>
2026-01-29 21:02:14 +0100

    changed gradle

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,7 +64,7 @@ compose.desktop {
 
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
 
-            packageVersion = "1.0.0"
+            packageVersion = project.findProperty("appVersion")?.toString() ?: "1.0.0"
             packageName = "Nuclear Option Mod Manager"
             vendor = "Combat"
             description = "A Mod Manager For Nuclear Option"
